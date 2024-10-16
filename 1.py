e02e1d5add0c6cf469fb579218304d093920de19
Valentinka68 <valay.krivitskaya@gmail.com>
2024-10-16 18:17:57 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,38 @@
+"""a=int(input())
+if a>14:
+    print("Добро пожаловать!")
+elif  a==14:
+    print("Вам 14 лет! Поздравляю!")
+else:
+    print("Вам еще рано!")
+
+
+a=input()
+if a=="secret":
+    print("Доступ разрешен!")
+else:
+    print("Неверный пароль!")
+
+
+a=int(input())
+if 500<=a<=1000:
+    a=a-(a*5/100)
+    print(a)
+elif a>1000:
+    a=a-(a*10/100)
+    print(a)
+
+
+a=input()
+if "0"<=a<="9":
+    print("Это цифра!")
+elif "a"<=a<="z" or "A"<=a<="Z":
+    print("Это буква!")
+else:
+    print("Это не цифра и не буква!")
+
+
+a,s,d,f,g=map(int,input().split())
+print(a+s+d+f+g)"""
+
+              
